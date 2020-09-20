511fa8dd7e8c5b21b60b1669815a3366818d4791
unknown <amitk2007@gmail.com>
2020-09-20 13:24:14 +0300

    added Bolder and Bolder Creator

---
+++ Assets/Scripts/Objects/BolderCreatorScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BolderCreatorScript : MonoBehaviour
+{
+    public GameObject bolder;
+    public Vector2 startForce;
+    [Range(0, 10)]
+    public float secondToWait;
+    static GameObject thisBolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBolder();
+        //WaitForSeconds(3);
+        StartCoroutine(CreateBolders(secondToWait));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator CreateBolders(float secondToWait)
+    {
+        while (true)
+        {
+            //Print the time of when the function is first called.
+            Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+            //yield on a new YieldInstruction that waits for 5 seconds.
+            yield return new WaitForSeconds(secondToWait);
+            CreateBolder();
+            //After we have waited 5 seconds print the time again.
+            Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        }
+    }
+
+    public void CreateBolder()
+    {
+        print("creating");
+        thisBolder = Instantiate(bolder);
+        thisBolder.GetComponent<Rigidbody2D>().AddForce(startForce);
+    }
+}
---
+++ Assets/Scripts/Objects/BolderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolderScript : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float TTL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyThisBolder(TTL));
+    }
+
+    IEnumerator DestroyThisBolder(float seconds)
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+    }
+}

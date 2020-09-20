ddd70991cd2c57f737c6267eb973429e6714db36
unknown <amitk2007@gmail.com>
2020-09-20 19:04:56 +0300

    climbing animtion added and opsi on the ladder lol

---
+++ Assets/Scripts/Objects/LadderScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+
+}
